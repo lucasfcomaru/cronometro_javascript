f194dfc146f531a1ce9163e6eac5b5e01000a985
lucasfcomaru <lukscomaru@gmail.com>
2024-07-23 12:21:51 -0300

    função iniciarTempo

---
+++ js/script.js
@@ -0,0 +1,49 @@
+// botões
+const iniciar = document.querySelector("[data-iniciar]");
+const pausar = document.querySelector("[data-pausar]");
+const parar = document.querySelector("[data-parar]");
+
+iniciar.addEventListener("click", iniciarTempo);
+pausar.addEventListener("click", pausarTempo);
+// parar.addEventListener("click", pararTempo);
+
+// espaços para os números
+const hora = document.querySelector("[data-hora]");
+const minuto = document.querySelector("[data-minuto]");
+const segundo = document.querySelector("[data-segundo]");
+
+//iniciando os contadores
+let segundos = 0;
+let minutos = 0;
+let horas = 0;
+
+// iniciando a variável que vai guardar o setInterval()
+let timer;
+
+function iniciarTempo() {
+  timer = setInterval(() => {
+    segundo.innerText = segundos++;
+
+    // verifica se segundos atingiu o valor de 60
+    //e acrescenta 1 ao minuto
+    if (segundos === 60) {
+      segundos = 0;
+      segundo.innerText = segundos;
+      minuto.innerText = ++minutos;
+    } else if (minutos === 60) {
+      //verifica se minutos atingiu o valor de 60
+      // e acrescenta 1 a horas
+      minutos = 0;
+      minuto.innerText = minutos;
+      hora.innerText = ++horas;
+    }
+  }, 5);
+
+  iniciar.setAttribute('disabled', ''); // desabilita o botão iniciar
+}
+
+function pausarTempo() {
+  clearInterval(timer); // para o setInterval()
+  iniciar.removeAttribute('disabled', ''); // habilita botão iniciar
+
+}
